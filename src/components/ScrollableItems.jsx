7a0fb9893ab6404ad809022898c3aacef2871971
Christian <64344406+cjxo@users.noreply.github.com>
2025-02-19 13:30:05 +0800

    Semi-working scroller: not 'pixel perfect' yet

--- src/components/ScrollableItems.jsx
+++ src/components/ScrollableItems.jsx
@@ -1,64 +1,93 @@
+import { useState } from "react";
 import styles from "../styles/component.module.css";
+import ButtonImage from "./ButtonImage";
 
-const ScrollableItems = () => {
-  /*
-    <div className={styles.scrollableItemsWrapper}>
-      <h2>Displayed Items</h2>
-      <div className={styles.scroller}>
-        <div className={styles.card}>
-          <h2>Hello</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vestibulum sollicitudin mi eu consectetur. Etiam ullamcorper gravida elementum. Curabitur varius nibh sapien, vel egestas nisi porta eget. Nulla nec nibh pretium, tempor leo et, lacinia eros. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent eu quam risus. Suspendisse suscipit lectus sit amet volutpat sollicitudin. 
-          </p>
-        </div>
-
-        <div className={styles.card}>
-          <h2>Hello</h2>
-          <p>
-            Sed vel odio vel massa dictum rutrum. Nulla convallis lacinia tempor. Nullam at ipsum lorem. Aliquam tempor, urna vel semper eleifend, nibh nibh aliquet erat, nec ultrices mi ante vitae arcu.
-          </p>
-        </div>
-
-        <div className={styles.card}>
-          <h2>Hello</h2>
-          <p>
-            Suspendisse potenti. Vestibulum suscipit faucibus massa vitae porttitor. Curabitur ac consequat sapien. Quisque condimentum et nunc in consequat. Cras ullamcorper, eros id rhoncus rhoncus, nisi sem molestie nisl, eget tincidunt neque elit vel orci. Aenean elementum in neque at viverra. Maecenas non nisi nibh. Ut non elementum sapien. Donec congue tellus nec efficitur suscipit.
-          </p>
-        </div>
-
-        <div className={styles.card}>
-          <h2>Hello</h2>
-          <p>
-            Vivamus congue lacus quis finibus scelerisque. Pellentesque ut sem sit amet ex convallis dictum eu at odio. Donec rhoncus felis eu consectetur gravida. Vivamus purus augue, rutrum nec diam in, laoreet facilisis massa. Nulla facilisi. Nulla et convallis nisl, non maximus ex. Curabitur eu ultrices odio. Nunc luctus suscipit dignissim. Maecenas scelerisque mollis massa, ultrices cursus purus convallis et. Maecenas vulputate risus nec tortor fermentum aliquet. Nullam eget varius metus, feugiat dignissim turpis. Morbi quis rutrum ipsum. Maecenas tincidunt odio at maximus molestie. Nullam quis ex sit amet lectus maximus laoreet nec eget ligula.
-          </p>
-        </div>
-
-        <div className={styles.card}>
-          <h2>Hello</h2>
-          <p>
-            Nulla imperdiet sapien eu semper tempus. Aliquam suscipit fermentum metus sed mollis. Donec id ligula vitae urna sagittis molestie. Quisque iaculis eleifend arcu, et euismod odio semper sit amet. Quisque tortor ante,
-          </p>
-        </div>
-
-        <div className={styles.card}>
-          <h2>Hello</h2>
-          <p>
-            Sed vel odio vel massa dictum rutrum. Nulla convallis lacinia tempor. Nullam at ipsum lorem. Aliquam tempor, urna vel semper eleifend, nibh nibh aliquet erat, nec ultrices mi ante vitae arcu.
-          </p>
-        </div>
-
-        <div className={styles.card}>
-          <h2>Hello</h2>
-          <p>
-            Suspendisse potenti. Vestibulum suscipit faucibus massa vitae porttitor. Curabitur ac consequat sapien. Quisque condimentum et nunc in consequat. Cras ullamcorper, eros id rhoncus rhoncus, nisi sem molestie nisl, eget tincidunt neque elit vel orci. Aenean elementum in neque at viverra. Maecenas non nisi nibh. Ut non elementum sapien. Donec congue tellus nec efficitur suscipit.
-          </p>
-        </div>
-      </div>
-    </div>
-    */
-  return (
-    <>TODO: ScrollableItems When We Hvae actual items!</>
+const ScrollableItems = ({ items }) => {
+  const [ithChild, setIthChild] = useState(0);
+  const handleScroll = ({ target }) => {
+    const parent = target.parentNode;
+    if (parent.nextSibling) {
+      const container = parent.nextSibling;
+      const containerAABB = container.getBoundingClientRect();
+      const firstChild = container.children[0];
+      
+      const firstChildWidth = firstChild.offsetWidth;
+      const borderWidthHalf = 0.5;
+      
+      if (firstChild.getBoundingClientRect().right > containerAABB.left) {
+        container.scrollLeft = (container.children.length - 1) * (firstChildWidth - borderWidthHalf);
+      } else {
+        container.scrollLeft += -firstChildWidth + borderWidthHalf;
+      }
+    } else {
+      const container = parent.previousSibling;
+      const containerAABB = container.getBoundingClientRect();
+      const firstChild = container.children[0];
+      const lastChild = container.children[container.children.length - 1];
+      
+      const firstChildWidth = firstChild.offsetWidth;
+      const borderWidthHalf = 0.5;
+      
+      if (lastChild.getBoundingClientRect().x < (containerAABB.right - lastChild.offsetWidth * 0.5)) {
+        container.scrollLeft = borderWidthHalf;
+      } else {        
+        container.scrollLeft += firstChildWidth + borderWidthHalf;
+      }
+    }
     
+    // https://stackoverflow.com/questions/66226428/use-css-modules-variables-in-javascript
+    //const styles = window.getComputedStyle(container).getPropertyValue("--slider-card-count");
+    //const width = styles.getPropertyValue("flex-basis");
+  };
+  
+  return (
+    <section className={`${styles.scrollableItemsContainer} common-container`}>
+      <div className="prologue">
+        <h2 className="title">Displayed Items</h2>
+      </div>
+      
+      <div className={styles.slider}>
+        <ButtonImage
+          src="./src/assets/svgrepo/left-svgrepo-com.svg"
+          alt="scroll left"
+          className={styles.scrollBtn}
+          onClick={handleScroll}
+        />
+        
+        <div className={styles.sliderWrapper}>
+          <div className={styles.sliderCard}>
+            A
+          </div>
+          
+          <div className={styles.sliderCard}>
+            B
+          </div>
+          
+          <div className={styles.sliderCard}>
+            C
+          </div>
+          
+          <div className={styles.sliderCard}>
+            D
+          </div>
+          
+          <div className={styles.sliderCard}>
+            E
+          </div>
+          
+          <div className={styles.sliderCard}>
+            F
+          </div>
+        </div>
+        
+        <ButtonImage
+          src="./src/assets/svgrepo/right-svgrepo-com.svg"
+          alt="scroll right"
+          className={styles.scrollBtn}
+          onClick={handleScroll}
+        />
+      </div>
+    </section>
   );
 };
 
