f67969f765fd35250d0aa22ebd196aca422edc3f
Christian <64344406+cjxo@users.noreply.github.com>
2025-02-19 15:25:09 +0800

    Basic Scroller Done

--- src/components/ScrollableItems.jsx
+++ src/components/ScrollableItems.jsx
@@ -1,28 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import styles from "../styles/component.module.css";
 import ButtonImage from "./ButtonImage";
 
 const ScrollableItems = ({ items }) => {
   const [ithChild, setIthChild] = useState(0);
+  const containerRef = useRef(null);
   
-  const handleScroll = ({ target }) => {
-    let parent;
-    if (target.tagName === "IMG") {
-      parent = target.parentNode;
-    } else {
-      parent = target;
-    }
+  const handleScroll = (scrollDir) => {
+    const container = containerRef.current;
+    const containerAABB = container.getBoundingClientRect();
+    const cardCount = window.getComputedStyle(container).getPropertyValue("--slider-card-count");
+    const borderWidthHalf = 0.5;
+    const scrollLimit = (container.children.length - 1) - (cardCount - 1);
     
-    if (parent.nextSibling) {
-      const container = parent.nextSibling;
-      const containerAABB = container.getBoundingClientRect();
+    if (scrollDir === "L") {
       const firstChild = container.children[0];
-      const borderWidthHalf = 0.5;
-      const cardCount = window.getComputedStyle(container).getPropertyValue("--slider-card-count");
-      
       if (container.children.length > cardCount) {
         if ((ithChild - 1) < 0) {
-          setIthChild((container.children.length - 1) - (cardCount - 1));
+          setIthChild(scrollLimit);
           container.scrollLeft = (container.children.length - 1) * (firstChild.offsetWidth - borderWidthHalf);
         } else {
           const ithChildNode = container.children[ithChild - 1];
@@ -30,16 +25,11 @@ const ScrollableItems = ({ items }) => {
           container.scrollLeft += ithChildNode.getBoundingClientRect().left - containerAABB.left + borderWidthHalf;
         }
       }
-    } else {
-      const container = parent.previousSibling;
-      const containerAABB = container.getBoundingClientRect();
+    } else if (scrollDir === "R") {
       const ithChildNode = container.children[ithChild + 1];
-      const lastChild = container.children[container.children.length - 1];
-      const borderWidthHalf = 0.5;
-      const cardCount = window.getComputedStyle(container).getPropertyValue("--slider-card-count");
       
       if (container.children.length > cardCount) {
-        if (((container.children.length - 1) - (cardCount - 1)) === ithChild) {
+        if (scrollLimit === ithChild) {
           container.scrollLeft = borderWidthHalf;
           setIthChild(0);
         } else {
@@ -54,6 +44,32 @@ const ScrollableItems = ({ items }) => {
     //const width = styles.getPropertyValue("flex-basis");
   };
   
+  useEffect(() => {
+    const snap = () => {
+      const container = containerRef.current;
+      /*
+      const containerAABB = container.getBoundingClientRect();
+      const cardCount = window.getComputedStyle(container).getPropertyValue("--slider-card-count");
+      const scrollLimit = (container.children.length - 1) - (cardCount - 1);
+      const firstChild = container.children[0];
+      const borderWidthHalf = 0.5;
+      
+      if (container.children.length > cardCount) {
+        if ((ithChild - 1) < 0) {
+          container.scrollLeft = (container.children.length - 1) * (firstChild.offsetWidth - borderWidthHalf);
+        } else {
+          const ithChildNode = container.children[ithChild - 1];
+          container.scrollLeft += ithChildNode.getBoundingClientRect().left - containerAABB.left + borderWidthHalf;
+        }
+      } else {
+      }*/
+      container.scrollLeft = 0;
+    };
+    
+    addEventListener("resize", snap);
+    return () => removeEventListener("resize", snap);
+  }, []);
+  
   return (
     <section className={`${styles.scrollableItemsContainer} common-container`}>
       <div className="prologue">
@@ -65,10 +81,10 @@ const ScrollableItems = ({ items }) => {
           src="./src/assets/svgrepo/left-svgrepo-com.svg"
           alt="scroll left"
           className={styles.scrollBtn}
-          onClick={handleScroll}
+          onClick={() => handleScroll("L")}
         />
         
-        <div className={styles.sliderWrapper}>
+        <div className={styles.sliderWrapper} ref={containerRef}>
           <div className={styles.sliderCard}>
             A
           </div>
@@ -98,7 +114,7 @@ const ScrollableItems = ({ items }) => {
           src="./src/assets/svgrepo/right-svgrepo-com.svg"
           alt="scroll right"
           className={styles.scrollBtn}
-          onClick={handleScroll}
+          onClick={() => handleScroll("R")}
         />
       </div>
     </section>
