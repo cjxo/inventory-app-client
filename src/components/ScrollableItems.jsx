0287468a1793c203b4d77c02ed0843dcdfa4b661
Christian <64344406+cjxo@users.noreply.github.com>
2025-02-19 14:56:27 +0800

    Achieved (HOPEFULLY) 'precise' scrolling

--- src/components/ScrollableItems.jsx
+++ src/components/ScrollableItems.jsx
@@ -4,34 +4,48 @@ import ButtonImage from "./ButtonImage";
 
 const ScrollableItems = ({ items }) => {
   const [ithChild, setIthChild] = useState(0);
+  
   const handleScroll = ({ target }) => {
-    const parent = target.parentNode;
+    let parent;
+    if (target.tagName === "IMG") {
+      parent = target.parentNode;
+    } else {
+      parent = target;
+    }
+    
     if (parent.nextSibling) {
       const container = parent.nextSibling;
       const containerAABB = container.getBoundingClientRect();
       const firstChild = container.children[0];
-      
-      const firstChildWidth = firstChild.offsetWidth;
       const borderWidthHalf = 0.5;
+      const cardCount = window.getComputedStyle(container).getPropertyValue("--slider-card-count");
       
-      if (firstChild.getBoundingClientRect().right > containerAABB.left) {
-        container.scrollLeft = (container.children.length - 1) * (firstChildWidth - borderWidthHalf);
-      } else {
-        container.scrollLeft += -firstChildWidth + borderWidthHalf;
+      if (container.children.length > cardCount) {
+        if ((ithChild - 1) < 0) {
+          setIthChild((container.children.length - 1) - (cardCount - 1));
+          container.scrollLeft = (container.children.length - 1) * (firstChild.offsetWidth - borderWidthHalf);
+        } else {
+          const ithChildNode = container.children[ithChild - 1];
+          setIthChild(ithChild - 1);
+          container.scrollLeft += ithChildNode.getBoundingClientRect().left - containerAABB.left + borderWidthHalf;
+        }
       }
     } else {
       const container = parent.previousSibling;
       const containerAABB = container.getBoundingClientRect();
-      const firstChild = container.children[0];
+      const ithChildNode = container.children[ithChild + 1];
       const lastChild = container.children[container.children.length - 1];
-      
-      const firstChildWidth = firstChild.offsetWidth;
       const borderWidthHalf = 0.5;
+      const cardCount = window.getComputedStyle(container).getPropertyValue("--slider-card-count");
       
-      if (lastChild.getBoundingClientRect().x < (containerAABB.right - lastChild.offsetWidth * 0.5)) {
-        container.scrollLeft = borderWidthHalf;
-      } else {        
-        container.scrollLeft += firstChildWidth + borderWidthHalf;
+      if (container.children.length > cardCount) {
+        if (((container.children.length - 1) - (cardCount - 1)) === ithChild) {
+          container.scrollLeft = borderWidthHalf;
+          setIthChild(0);
+        } else {
+          setIthChild(ithChild + 1);
+          container.scrollLeft += ithChildNode.getBoundingClientRect().left - containerAABB.left + borderWidthHalf;
+        }
       }
     }
     
